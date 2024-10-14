29ae6449df5159d30887c305597c6fc3dc96a3a6
TercanoNuruva <thyoung89@gmail.com>
2024-10-14 14:34:06 -0600

    snake + pong

---
+++ week_3/working.py
@@ -0,0 +1,43 @@
+# snake = Turtle("square")
+# snake.color('white')
+# snake2 = Turtle("square")
+# snake2.color("white")
+# snake.forward(20)
+# snake3 = Turtle("square")
+# snake3.color("white")
+# snake2.back(20)
+
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+#
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+#
+#     def breathe(self):
+#         print("Inhale, exhale.")
+#
+# class Fish(Animal):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def breathe(self):
+#         super().breathe()
+#         print("doing this underwater.")
+#
+#     def swim(self):
+#         print("moving in water")
+#
+# nemo = Fish()
+# nemo.swim()
+# nemo.breathe()
+# print(nemo.num_eyes)
+
